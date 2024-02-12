9e96d92bb8347593bac68a09c7b42073e9ae2ffe
Pascal Berger <pascal.berger@helbling.ch>
2024-02-12 08:53:02 +0100

    added failsafe, new unpublished version

--- pythi/pythi.py
+++ pythi/pythi.py
@@ -1,15 +1,24 @@
 import os
 
-def unify_cwd():
+def unify_cwd(root_identifier = ".git"):
   """
-  cd into the project root folder (containing the .git subfolder)
+  cd into the project root folder
 
-  :return: None
+  Argments:
+  - root_identifier: str folder or file path in the root directory used for identification
+
+  Returns:
+  - None
   """
   cwd = os.getcwd()
-  while(not os.path.exists(os.path.join(cwd, '.git'))):
-    cwd = os.path.dirname(cwd)
 
+  while(not os.path.exists(os.path.join(cwd, root_identifier))):
+    if(cwd == os.path.dirname(cwd)):
+      raise Exception(f"Could not find project root containing <{root_identifier}>.")
+
+    cwd = os.path.dirname(cwd)
+  
   # change cwd to this directory
   os.chdir(cwd)
-  
+    
+
