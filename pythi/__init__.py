611fb1b3c143874034e9e888cd3b469359f528c2
Pascal Berger <pascal.berger@helbling.ch>
2024-06-05 13:52:09 +0200

    added df memory size

--- pythi/__init__.py
+++ pythi/__init__.py
@@ -2,4 +2,4 @@
 from .pythi import unify_cwd
 from .downloader import download_parallel, download
 
-__all__ = ['unify_cwd', 'download_parallel', 'download']
+__all__ = ['unify_cwd', 'get_df_size', 'download_parallel', 'download']
--- pythi/pythi.py
+++ pythi/pythi.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def unify_cwd(root_identifier = ".git"):
   """
@@ -20,5 +21,21 @@ def unify_cwd(root_identifier = ".git"):
   
   # change cwd to this directory
   os.chdir(cwd)
+
+def get_df_size(df):
+  """
+  Print the size of a pandas DataFrame in memory
+  
+  Arguments:
+  - df: pd.DataFrame
+
+  Returns:
+  - None
+  """
+
+  print(f"{sys.getsizeof(df)/1024/1024/1024:.1f} GB")
+  # df Grösse nach Spalte
+  for column in df.columns:
+      print(f"{column}: {sys.getsizeof(df[[column]])/1024/1024:.1f} MB")
     
 
